e831773fad8279f0cdf45f0558b86af0cf5df680
LoRut0 <yaemkapustu37@gmail.com>
2025-09-25 13:26:00 +0300

    server is working, but does not handle cursed errors, also deleted exceccive files

--- src/userver/final/redis.cpp
+++ src/userver/final/redis.cpp
@@ -19,6 +19,11 @@ namespace services::websocket {
         return 201;
     }
 
+    int WebsocketsHandler::redisPostJson(std::string_view key, const std::string &jsonString) const {
+        redis_client_->Set(std::string{key}, jsonString, redis_cc_).Get();
+        return 201;
+    }
+
     size_t WebsocketsHandler::redisDelete(std::string_view key) const {
         const size_t result = redis_client_->Del(std::string{key}, redis_cc_).Get();
         return result;
